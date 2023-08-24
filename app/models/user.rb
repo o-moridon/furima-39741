71778bfe5de2418d2f354d3a5ae8df4495b39104
o-moridon <ohmori.t_0921@icloud.com>
2023-08-24 20:33:27 +0900

    エラーメッセージ重複の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
 
         
   validates :nickname,                  presence: true
-  validates :email,                     presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password,                  presence: true, length: { minimum: 6 }
   validates :password_confirmation,     presence: true
   validates :family_name,               presence: true
   validates :first_name,                presence: true
