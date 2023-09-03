b9527d3357a01e61ed9c52c2561c908807166534
o-moridon <ohmori.t_0921@icloud.com>
2023-09-03 14:57:28 +0900

    クレジット決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,16 +1,22 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
   def new
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
+    if @item.purchase || current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
+    @item = Item.find(params[:item_id])
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :new, status: :unprocessable_entity
     end
   end
@@ -18,7 +24,15 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :street_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :street_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id).merge(token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,16 +1,18 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :post_code, :user_id, :prefecture_id, :city, :street_number, :building_name, :phone_number, :purchase_id
+  attr_accessor :item_id, :post_code, :user_id, :prefecture_id, :city, :street_number, :building_name, :phone_number, :purchase_id, :token
 
   with_options presence: true do
     validates :item_id
     validates :user_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
     validates :city
     validates :street_number
-    validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/}
+    validates :token
   end
+
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
